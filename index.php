2557d23c77c9ae089e48e7c41f883ac7a21379b7
NelloRossi <rossianiello97@gmail.com>
2023-05-08 14:44:17 +0200

    added basic structure

--- index.php
+++ index.php
@@ -12,7 +12,15 @@
   </head>
   <body>
     <div id="app">
-    
+        <div class="container text-center">
+            <h1>To Do List</h1>
+            <div class="container">
+                <ul class="list-unstyled">
+                    <li>item</li>
+                </ul>
+            </div>
+        </div>
+        
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
